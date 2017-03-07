3da19e984416f223b3f85e5af333990b7e8c941d
Manuel Baumgartner <manuel.baumgartner@student.hs-rm.de>
2017-03-07 21:39:47 +0100

    (un)bind cam instead of start/stop service
    stopping motion service will make motioneye completely unavailable. In case you use more than one cam, all cams will get disabled.
    
    you can get the video device via:
    
    $ ls -l /sys/bus/usb/drivers/uvcvideo/
    
    then you select your device and call bind/unbind

--- usr/sbin/motionPhone.sh
+++ usr/sbin/motionPhone.sh
@@ -13,12 +13,16 @@ if systemctl -q is-active motioneye.service ; then
   # service is up
   if [[ $phone_present -eq 1 ]] ; then
     # someone is home, stop service
-    systemctl stop motioneye.service
+    #systemctl stop motioneye.service
+    # instead of stopping the service, unbind cam
+     echo '1-1.4:1.0' | sudo tee /sys/bus/usb/drivers/uvcvideo/unbind  > /dev/null 2>&1
   fi
 else
   # service is down
   if [[ $phone_present -eq 0 ]] ; then
     # phone not available, start service
-    systemctl start motioneye.service
+    #systemctl start motioneye.service
+    # bind cam
+    echo '1-1.4:1.0' | sudo tee /sys/bus/usb/drivers/uvcvideo/bind  > /dev/null 2>&1 
   fi
 fi
