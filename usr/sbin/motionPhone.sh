c834dd9a787672af64f551217ce7c3d69d0e5d95
Manuel Baumgartner <manuel.baumgartner@student.hs-rm.de>
2017-03-07 23:05:58 +0100

    multiple ips

--- usr/sbin/motionPhone.sh
+++ usr/sbin/motionPhone.sh
@@ -1,19 +1,21 @@
-
 #!/bin/bash
 
-# ip that will be pinged
-phone_ip="192.168.0.101"
+# ips that will be pinged
+ips=("192.168.0.100" "192.168.0.101") 
 
-phone_present=0
+present=0
 
-ping -c 1 $phone_ip &>/dev/null && phone_present=1
+for ip in "${ips[@]}"
+do
+    ping -c 1 $ip & > /dev/null && present=1
+done
 
-if [[ $phone_present -eq 1 ]] ; then
+if [[ $present -eq 1 ]] ; then
      # unbind cam
      echo '1-1.4:1.0' | sudo tee /sys/bus/usb/drivers/uvcvideo/unbind  > /dev/null 2>&1
 fi
 
-if [[ $phone_present -eq 0 ]] ; then
+if [[ $present -eq 0 ]] ; then
     # bind cam
     echo '1-1.4:1.0' | sudo tee /sys/bus/usb/drivers/uvcvideo/bind  > /dev/null 2>&1 
 fi
