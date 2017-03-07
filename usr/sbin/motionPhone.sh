2148d44b9e66a7a95b56544c86738a7f80cd3439
Manuel Baumgartner <manuel.baumgartner@student.hs-rm.de>
2017-03-07 22:26:07 +0100

    Update motionPhone.sh

--- usr/sbin/motionPhone.sh
+++ usr/sbin/motionPhone.sh
@@ -1,28 +1,19 @@
+
 #!/bin/bash
 
-#found here: https://github.com/ccrisan/motioneye/issues/203
-
+# ip that will be pinged
 phone_ip="192.168.0.101"
 
-#check for phone
 phone_present=0
 
 ping -c 1 $phone_ip &>/dev/null && phone_present=1
 
-if systemctl -q is-active motioneye.service ; then
-  # service is up
-  if [[ $phone_present -eq 1 ]] ; then
-    # someone is home, stop service
-    #systemctl stop motioneye.service
-    # instead of stopping the service, unbind cam
+if [[ $phone_present -eq 1 ]] ; then
+     # unbind cam
      echo '1-1.4:1.0' | sudo tee /sys/bus/usb/drivers/uvcvideo/unbind  > /dev/null 2>&1
-  fi
-else
-  # service is down
-  if [[ $phone_present -eq 0 ]] ; then
-    # phone not available, start service
-    #systemctl start motioneye.service
+fi
+
+if [[ $phone_present -eq 0 ]] ; then
     # bind cam
     echo '1-1.4:1.0' | sudo tee /sys/bus/usb/drivers/uvcvideo/bind  > /dev/null 2>&1 
-  fi
 fi
