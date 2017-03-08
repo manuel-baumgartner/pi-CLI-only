2c53075170f05f00d4e5b277a5bfc55e5d773028
Manuel Baumgartner <manuel.baumgartner@student.hs-rm.de>
2017-03-08 01:33:45 +0100

    Create install.sh

---
+++ install/install.sh
@@ -0,0 +1,12 @@
+#bin/bash
+
+#vars
+$INSTALL="/tmp"  # installation folder
+
+cd $INSTALL
+sudo apt-get update & sudo apt-get -y upgrade
+
+mkdir install
+cd install
+wget https://raw.githubusercontent.com/manuel-baumgartner/pi-CLI-only/master/install/plex.sh
+plex.sh
