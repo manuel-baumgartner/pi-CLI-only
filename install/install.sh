1b27dbcd64a0044824defb38d68fc746ac969a30
Manuel Baumgartner <manuel.baumgartner@student.hs-rm.de>
2017-03-08 01:51:07 +0100

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -8,6 +8,13 @@ sudo apt-get update & sudo apt-get -y upgrade
 
 mkdir install
 cd install
+
+# plex
 wget https://raw.githubusercontent.com/manuel-baumgartner/pi-CLI-only/master/install/plex.sh
-sudo chmod +x plex.sh
+chmod +x plex.sh
 ./plex.sh
+
+# motioneye
+wget https://raw.githubusercontent.com/manuel-baumgartner/pi-CLI-only/master/install/motioneye.sh
+chmod +x motioneye.sh
+./motioneye.sh
