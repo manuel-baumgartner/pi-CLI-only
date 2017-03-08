c700d127dc102f19900017aaae6aca320074876d
Manuel Baumgartner <manuel.baumgartner@student.hs-rm.de>
2017-03-08 02:24:20 +0100

    Update motionPhone.sh

--- usr/sbin/motionPhone.sh
+++ usr/sbin/motionPhone.sh
@@ -12,10 +12,10 @@ done
 
 if [[ $present -eq 1 ]] ; then
      # unbind cam
-     echo '1-1.4:1.0' | sudo tee /sys/bus/usb/drivers/uvcvideo/unbind  > /dev/null 2>&1
+     echo '1-1.5:1.0' | sudo tee /sys/bus/usb/drivers/uvcvideo/unbind  > /dev/null 2>&1
 fi
 
 if [[ $present -eq 0 ]] ; then
     # bind cam
-    echo '1-1.4:1.0' | sudo tee /sys/bus/usb/drivers/uvcvideo/bind  > /dev/null 2>&1 
+    echo '1-1.5:1.0' | sudo tee /sys/bus/usb/drivers/uvcvideo/bind  > /dev/null 2>&1 
 fi
