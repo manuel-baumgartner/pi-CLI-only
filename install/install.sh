30d4a23011c659debec4c0c47cc73428e9602e16
Manuel Baumgartner <manuel.baumgartner@student.hs-rm.de>
2017-03-08 01:37:54 +0100

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -9,4 +9,5 @@ sudo apt-get update & sudo apt-get -y upgrade
 mkdir install
 cd install
 wget https://raw.githubusercontent.com/manuel-baumgartner/pi-CLI-only/master/install/plex.sh
+sudo chmod +x plex.sh
 ./plex.sh
