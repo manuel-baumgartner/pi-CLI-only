2fcda544e601ea783498b69671fdd456186a8b4e
Manuel Baumgartner <manuel.baumgartner@student.hs-rm.de>
2017-03-08 01:47:03 +0100

    Update motioneye.sh

--- install/motioneye.sh
+++ install/motioneye.sh
@@ -3,16 +3,16 @@ cd /tmp
 
 # ffmpeg if not installed
 wget https://github.com/ccrisan/motioneye/wiki/precompiled/ffmpeg_3.1.1-1_armhf.deb
-dpkg -i ffmpeg_3.1.1-1_armhf.deb
+sudo dpkg -i ffmpeg_3.1.1-1_armhf.deb
 
 sudo apt-get remove libavcodec-extra-56 libavformat56 libavresample2 libavutil54
 
 # install dependencies
-sudo apt-get install python-pip python-dev curl libssl-dev libcurl4-openssl-dev libjpeg-dev libx264-142 libavcodec56 libavformat56 libmysqlclient18 libswscale3 libpq5
+sudo apt-get -y install python-pip python-dev curl libssl-dev libcurl4-openssl-dev libjpeg-dev libx264-142 libavcodec56 libavformat56 libmysqlclient18 libswscale3 libpq5
 
 # install motion
 wget https://github.com/Motion-Project/motion/releases/download/release-4.0.1/pi_jessie_motion_4.0.1-1_armhf.deb
-dpkg -i pi_jessie_motion_4.0.1-1_armhf.deb
+sudo dpkg -i pi_jessie_motion_4.0.1-1_armhf.deb
 
 # install motion eye
 sudo pip install motioneye
