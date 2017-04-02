979f01521ef5ff82c18da71dba33654bffe80322
Manuel Baumgartner <manuel.baumgartner@student.hs-rm.de>
2017-04-02 11:06:37 +0200

    Create lamp.sh

---
+++ install/lamp.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo apt-get install apache2 php5 libapache2-mod-php5 
+sudo service apache2 restart
+sudo apt-get install mysql-server mysql-client php5-mysql
+sudo apt-get install phpmyadmin
