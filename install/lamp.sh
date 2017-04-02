3e570fdf2d0b37467f4de405863d028fae2d895c
Manuel Baumgartner <manuel.baumgartner@student.hs-rm.de>
2017-04-02 12:02:47 +0200

    Update lamp.sh

--- install/lamp.sh
+++ install/lamp.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-sudo apt-get install apache2 php5 libapache2-mod-php5 
+sudo apt-get -y install apache2 php5 libapache2-mod-php5 
 sudo service apache2 restart
-sudo apt-get install mysql-server mysql-client php5-mysql
-sudo apt-get install phpmyadmin
+sudo apt-get -y install mysql-server mysql-client php5-mysql
+sudo apt-get -y install phpmyadmin
+sudo ln -s /etc/phpmyadmin/apache.conf /etc/apache2/conf-available/phpmyadmin.conf
+sudo a2enconf phpmyadmin
+sudo service apache2 reload
