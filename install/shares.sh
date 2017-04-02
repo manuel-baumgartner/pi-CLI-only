98df2a4a9a5f634cdbf217ebaf8453ab64221f40
Manuel Baumgartner <manuel.baumgartner@student.hs-rm.de>
2017-04-02 12:47:28 +0200

    enable rpcbind

--- install/shares.sh
+++ install/shares.sh
@@ -1,12 +1,13 @@
 #!bin/bash
 
 # nfs
-sudo apt-get install nfs-kernel-server
+sudo apt-get -y install nfs-kernel-server
 # nfs fix
 wget https://raw.githubusercontent.com/manuel-baumgartner/pi-CLI-only/master/etc/systemd/system/nfs-common.services
 cp nfs-common.services /etc/system/systemd
 wget https://raw.githubusercontent.com/manuel-baumgartner/pi-CLI-only/master/etc/systemd/system/rpcbind.service
 cp rpcbind.service /etc/systemd/system
+sudo update-rc.d rpcbind enable
 
 # samba
-sudo apt-get install samba
+sudo apt-get -y install samba
